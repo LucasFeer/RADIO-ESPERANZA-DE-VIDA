53b1b583142255fb3addb05e43e6d2f7759afb8e
Radio edv <radioedv@github.com>
2019-04-11 12:22:16 -0300

    Pagina Principal

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.9.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.9.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-128x128-1.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Inicio</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-0">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    
+                         <img src="assets/images/logo-122x122.jpg" alt="" title="" style="height: 3.8rem;">
+                    
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://iglesiaesperanzadevida.mobirisesite.com/" target="_blank">BIENVENIDOS<br></a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="tel:+54 11 6181-1905"><span class="mbri-users mbr-iconfont mbr-iconfont-btn"></span>
+                        Sobre Nosotros&nbsp; &nbsp;&nbsp;</a>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/l">free website templates</a></section><section class="cid-qTkA127IK8 mbr-fullscreen mbr-parallax-background" id="header2-1">
+
+    
+
+    
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">Ministerio Esperanza De Vida</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">El pastor Ramon Ferreyra y la Iglesia en su conjunto te dan la bienvenida a compartir con Nostros la radio " Esperanza de Vida"<br><br></p>
+                
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="cid-rlSD3BsEcK" id="social-buttons2-7">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-md-8 align-center">
+                <h2 class="pb-3 mbr-fonts-style display-2"><em><strong>UBICANOS</strong></em></h2>
+                <div class="social-list pl-0 mb-0">
+                    <a href="https://www.youtube.com/channel/UCc998W_EeSb5eMd4PODcuxw" target="_blank">
+                        <span class="px-2 mbr-iconfont mbr-iconfont-social socicon-youtube socicon" style="color: rgb(253, 8, 8); fill: rgb(253, 8, 8); font-size: 60px;"></span>
+                    </a>
+                    <a href="https://www.facebook.com/esperanzadevida.iglesia" target="_blank">
+                        <span class="px-2 mbr-iconfont mbr-iconfont-social socicon-facebook socicon" style="font-size: 60px; color: rgb(20, 157, 204); fill: rgb(20, 157, 204);"></span>
+                    </a>
+                    <a href="https://chat.whatsapp.com/K11yuOREFQO3A6Pt6JaaGt" target="_blank">
+                        <span class="px-2 mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon" style="font-size: 60px; color: rgb(0, 245, 32); fill: rgb(0, 245, 32);"></span>
+                    </a>
+                    <a href="https://www.youtube.com/c/mobirise" target="_blank">
+                        
+                    </a>
+                    <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                        
+                    </a>
+                    <a href="https://www.behance.net/Mobirise" target="_blank">
+                        
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section contacts1 cid-rlSCKl2NDu" id="contacts1-4">
+    <!---->
+    
+    <!---->
+    <!--Overlay-->
+    
+    <!--Container-->
+    <div class="container">
+        <div class="row">
+            <!--Titles-->
+            <div class="title col-12">
+                <h2 class="align-left mbr-fonts-style display-1"><strong>¡Contactate &nbsp;con Nosotros!</strong></h2>
+                
+            </div>
+            <!--Left-->
+            <div class="col-12 col-md-6">
+                <div class="left-block wrapper">
+                    <div class="b b-adress">
+                        <h5 class="align-left mbr-fonts-style m-0 display-5">
+                            Dirección:</h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7">
+                            Kellertas 1769 - Longchamps<br><br><em>Buenos Aires, Argentina</em></p>
+                    </div>
+                    <div class="b b-phone">
+                        <h5 class="align-left mbr-fonts-style m-0 display-5">Correo<br></h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7">ministerioesperanzadvida@gmail.com</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <!--Right-->
+            <div class="col-12 col-md-6">
+                <div class="google-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!4v1553656815370!6m8!1m7!1sMpdmH3bTu3xP_pVAwJD_fg!2m2!1d-34.86949173120555!2d-58.38421775650398!3f214.3142190399878!4f1.9725614890132448!5f0.7820865974627469" allowfullscreen=""></iframe></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/sociallikes/social-likes.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+  </body>
+</html>
